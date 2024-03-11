ee4b86976fde341ada62d7b818dd40a5a9d6a603
Anns Shahbaz <annsshahbaz@gmail.com>
2024-03-11 16:03:44 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,8 +13,20 @@ namespace PFDumy
             Console.WriteLine("Anns RG");
             Console.WriteLine("Anns RG");
             Console.WriteLine("Anns RG");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
             Console.WriteLine("Anns RG");
             Console.WriteLine("Anns RG");
+        }
+
+        public static void FunctionByAnnsGmail()
+        {
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
         }
     }
 }
