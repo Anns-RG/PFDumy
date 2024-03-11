e768fdcd3bf9e371f08f09e1f886cf597a6a3836
Anns-RG <anns@revolvinggames.com>
2024-03-11 16:02:33 +0500

    added functoin

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
-﻿namespace PFDumy
+﻿using System;
+namespace PFDumy
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+        }
+
+        public static void FunctionByAnnsRG()
+        {
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
         }
     }
 }
