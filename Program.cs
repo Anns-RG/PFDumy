b72286183af419a596699de086f7eb3ae4aa4985
Anns Shahbaz <141120958+Anns-RG@users.noreply.github.com>
2024-03-11 16:05:26 +0500

    [TK-1] PR by RG (#2)
    * added functoin
    
    * [TK-0] Function Gmail #1
    
    ---------
    
    Co-authored-by: Anns Shahbaz <annsshahbaz@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
-﻿namespace PFDumy
+﻿using System;
+namespace PFDumy
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+        }
+
+        public static void FunctionByAnnsRG()
+        {
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Anns RG");
+            Console.WriteLine("Anns RG");
+        }
+
+        public static void FunctionByAnnsGmail()
+        {
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
+            Console.WriteLine("Line by Anns Gmail");
         }
     }
 }
