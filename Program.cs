6594e479b794aa7bd01dd7ab7e73caebc626717b
Anns Shahbaz <141120958+Anns-RG@users.noreply.github.com>
2024-03-11 16:18:36 +0500

    [TK-00000] (#3)
    --------
    
    Co-authored-by: Anns Shahbaz <annsshahbaz@gmail.com>

--- Program.cs
+++ Program.cs
@@ -7,26 +7,17 @@ namespace PFDumy
         {
         }
 
-        public static void FunctionByAnnsRG()
+        public static void FunctionByAnnsRGInFeature()
         {
-            Console.WriteLine("Anns RG");
-            Console.WriteLine("Anns RG");
-            Console.WriteLine("Anns RG");
-            Console.WriteLine("Anns RG");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Anns RG");
-            Console.WriteLine("Anns RG");
-        }
-
-        public static void FunctionByAnnsGmail()
-        {
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
-            Console.WriteLine("Line by Anns Gmail");
+            //// A line from RG
+            // // A line from RGA line from RG
+            // A line from RG
+            // A line from RG
+            // A line from RG// A line from RG
+            // A line from RG
+            // A line from RG
+            // A line from RG
+            Console.WriteLine("");
         }
     }
 }
